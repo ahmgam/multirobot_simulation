c122a1e145190def1fb1892ebf9412a4149a4bf2
ahmgam <ahmgam@protonmail.com>
2024-08-23 08:25:53 +0300

    chore: Update Dockerfile and entrypoint.sh for simulation setup

--- simulation/entrypoint.sh
+++ simulation/entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 source "/usr/share/gazebo/setup.sh" --
 source "/opt/ros/noetic/setup.bash" --
 source "/root/robot_ws/devel/setup.bash" --
-
+npm run deploy --- -m
+cp -R /root/robot_ws/src/turtlebot3/turtlebot3_description /root/gzweb/http/client/assets/turtlebot3_description
+cp -R /root/robot_ws/src/rotor_simulations/rotors_description /root/gzweb/http/client/assets/rotors_description
 chmod -R +rx /root/robot_ws/src/multirobot_sim
 roscore & roslaunch --wait /root/robot_ws/src/multirobot_sim/launch/world-map-docker.launch & npm start
