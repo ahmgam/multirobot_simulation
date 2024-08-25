649a8794ede870bbe1572aafad293e7fdca8511e
ahmgam <ahmgam@protonmail.com>
2024-08-25 03:09:07 +0300

    Refactor Dockerfile to include ros-noetic-gazebo-msgs dependency

--- simulation/entrypoint.sh
+++ simulation/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 source "/usr/share/gazebo/setup.sh" --
 source "/opt/ros/noetic/setup.bash" --
 source "/root/robot_ws/devel/setup.bash" --
-npm run deploy --- -m
+#npm run deploy --- -m
 cp -R /root/robot_ws/src/turtlebot3/turtlebot3_description /root/gzweb/http/client/assets/turtlebot3_description
 cp -R /root/robot_ws/src/rotor_simulations/rotors_description /root/gzweb/http/client/assets/rotors_description
 chmod -R +rx /root/robot_ws/src/multirobot_sim
