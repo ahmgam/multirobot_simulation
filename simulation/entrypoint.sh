830cdfed968b927d8bb4ada38ac816ad83d6e480
ahmgam <ahmgam@protonmail.com>
2024-08-25 04:54:58 +0300

    Refactor Dockerfile to include ros-noetic-desktop-full dependency

--- simulation/entrypoint.sh
+++ simulation/entrypoint.sh
@@ -3,8 +3,5 @@ set -e
 source "/usr/share/gazebo/setup.sh" --
 source "/opt/ros/noetic/setup.bash" --
 source "/root/robot_ws/devel/setup.bash" --
-#npm run deploy --- -m
-cp -R /root/robot_ws/src/turtlebot3/turtlebot3_description /root/gzweb/http/client/assets/turtlebot3_description
-cp -R /root/robot_ws/src/rotor_simulations/rotors_description /root/gzweb/http/client/assets/rotors_description
 chmod -R +rx /root/robot_ws/src/multirobot_sim
-roscore & roslaunch --wait /root/robot_ws/src/multirobot_sim/launch/world-map-docker.launch & npm start
+roscore & roslaunch --wait /root/robot_ws/src/multirobot_sim/launch/world-map-docker.launch > /dev/stdout 2> /dev/stderr
