cee218fa3b4328bd62ba57ca10c8b25e29827871
ahmgam <ahmgam@protonmail.com>
2024-08-25 04:04:23 +0300

    chore: Add sleep to wait for roscore to start in entrypoint.sh

--- nodes/entrypoint.sh
+++ nodes/entrypoint.sh
@@ -5,6 +5,8 @@ chmod +x -R /robot_ws/src/multirobot_sim/scripts
 # setup ros environment
 source "/opt/ros/noetic/setup.bash" --
 source "/robot_ws/devel/setup.bash" --
+# sleep 20 seconds to wait for roscore to start
+sleep 20
 #run the startup script
 python3 /robot_ws/src/multirobot_sim/scripts/roschain/startup.py
 #roslaunch rotors_gazebo uav_launch.launch
